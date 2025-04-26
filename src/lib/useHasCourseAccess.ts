72dfa967c4971180d2b647ce422ab806c91b2787
Роман Мухаметшин <roman.muhametshin@triafly.ru>
2025-04-26 22:03:08 +0300

    fix

--- src/lib/useHasCourseAccess.ts
+++ src/lib/useHasCourseAccess.ts
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import { useCheckCourseAccessQuery } from "../api/userApi";
 
 export const useHasCourseAccess = () => {
   const token = localStorage.getItem("token");
 
-  const { data, isLoading } = useCheckCourseAccessQuery(undefined, {
+  const { data, isLoading, error } = useCheckCourseAccessQuery(undefined, {
     skip: !token,
   });
+
+  useEffect(() => {
+    if (error && "status" in error && error.status === 404) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("isAccessKey");
+    }
+  }, [error]);
 
   const hasAccess = !!token && !!data?.hasAccess;
 
