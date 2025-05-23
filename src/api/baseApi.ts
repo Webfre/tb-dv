a870f5e7ff8fbd0c1197d5d60aa1dec8426aa22d
Роман Мухаметшин <roman.muhametshin@triafly.ru>
2025-05-23 10:09:11 +0300

    v1.2

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: process.env.REACT_APP_API_URL || "http://localhost:8000",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
