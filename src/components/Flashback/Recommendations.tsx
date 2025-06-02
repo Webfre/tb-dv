fb714e0c55d44d6e1ec667fd74d37810ebc6c286
Роман Мухаметшин <roman.muhametshin@triafly.ru>
2025-06-02 22:46:33 +0300

    v1.2

--- src/components/Flashback/Recommendations.tsx
+++ src/components/Flashback/Recommendations.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Typography, Box } from "@mui/material";
 import { handleSectionClick } from "../../lib/handleSectionClick";
+import { useCheckCourseAccessQuery } from "../../api/userApi";
+import { hasAccessToCourses } from "../../lib/hasAccessToCourses";
 
 interface RecommendationsProps {
   modules: { name: string; id: string }[];
@@ -11,6 +13,10 @@ const Recommendations: React.FC<RecommendationsProps> = ({
   modules,
   sections,
 }) => {
+  const { data } = useCheckCourseAccessQuery();
+  const accessCourses = data?.accessCourse || [];
+  console.log(accessCourses);
+
   return (
     <Box mt={4} p={2} bgcolor="#f5f5f5" borderRadius="20px">
       <Typography variant="h6" gutterBottom>
@@ -40,7 +46,9 @@ const Recommendations: React.FC<RecommendationsProps> = ({
               key={idx}
               color="primary"
               style={{ cursor: "pointer", textDecoration: "underline" }}
-              onClick={() => handleSectionClick(sec.moduleId, sec.topicId)}
+              onClick={() =>
+                handleSectionClick(sec.moduleId, sec.topicId, accessCourses)
+              }
             >
               {sec.name}
             </Typography>
