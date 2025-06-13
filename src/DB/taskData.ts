362d689a31fa2d3420a2831eba63e23ee98383d3
Oleg <mitinom98@gmail.com>
2025-06-13 13:28:25 +0300

    final-verstka

--- src/DB/taskData.ts
+++ src/DB/taskData.ts
@@ -1,6 +1,8 @@
 import { dataTaskCss } from "./CSS/DATA_COURSE/TASK";
 import { htmlTaskData } from "./HTML/DATA_COURSE/TASK";
 import { taskDataJs } from "./JAVASCRIPT/DATA_COURSE/TASK";
+import { dataTaskScss } from "./SCSS/DATA_COURSE/TASK";
+import { dataTaskFinal } from "./VERSKA_FINAL/TASK";
 import { PracticeTask } from "./index_type";
 
 export const practiceMock: PracticeTask[] = [
@@ -10,6 +12,11 @@ export const practiceMock: PracticeTask[] = [
   // CSS course
   ...dataTaskCss,
 
+  // SCSS
+  ...dataTaskScss,
+
   // JS
   ...taskDataJs,
+
+  ...dataTaskFinal,
 ];
